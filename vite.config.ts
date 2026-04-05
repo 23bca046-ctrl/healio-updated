ff8e50951362851f309104bf84c55c9ff02de333
23bca046-ctrl <23bca046@ncuindia.edu>
2026-04-05 17:32:27 +0530

    Refactor vite.config.ts to remove __dirname
    Removed __dirname definition for ES modules.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-
-// Fix: Define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
